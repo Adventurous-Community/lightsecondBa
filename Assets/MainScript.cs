f5af1511595cb7cfadf897c335356abe55e4233d
eivkov <e.ivkov@mail.ru>
2016-06-07 19:13:56 +0300

    Base

---
+++ Assets/MainScript.cs
@@ -0,0 +1,146 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public abstract class Device
+{
+
+    public string label;
+    public string description;
+    public int hp;
+    public int max_hp;
+    public int energy_usage;
+    public bool is_firing;
+    public int d_type;
+    public GameObject unityShip;
+    public GameObject unityDevice;
+    public abstract void Fire();
+
+}
+
+public class Weapon: Device
+{
+    public int w_range;
+
+    public override void Fire()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class Drive: Device{
+	public int speed;
+    public float r_speed;
+    public Vector3 new_position;
+    public Quaternion new_rotation;
+
+    public override void Fire(){
+        Vector3 direction = new_position - unityShip.transform.position;
+        float r_angle = new_rotation.z - unityShip.transform.rotation.z;
+        if(r_angle > 180)
+        {
+            unityShip.transform.rotation.Set(0, 0, r_speed,0);
+
+        }
+		direction.Normalize ();
+		direction *= speed;
+		unityShip.transform.Translate(direction*Time.deltaTime);
+        
+	}
+}
+
+public class Spaceship
+{
+    public string log;
+    public Vector3 position;
+    public Vector3 new_position;
+    public Quaternion rotation;
+    public Quaternion new_rotation;
+    public GameObject unityShip;
+    public int energy;
+    public int max_energy;
+    public int hp;
+    public int max_hp;
+    public ArrayList weapons;//оружие
+    public ArrayList sciDevices;//Научные девайсы(для лечения)
+    public ArrayList superWeapons;//супероружие
+    public ArrayList drives;//Двигатели
+
+    public Spaceship(GameObject us, int m_hp, int m_en)
+    {
+        position = us.transform.position;
+        new_position = position;
+        rotation = us.transform.rotation;
+        new_rotation = rotation;
+        unityShip = us;
+        max_hp = m_hp;
+        hp = max_hp;
+        energy = m_en;
+        max_energy = m_en;
+        weapons = new ArrayList();
+        sciDevices = new ArrayList();
+        superWeapons = new ArrayList();
+        drives = new ArrayList();
+        log = "started";
+    }
+
+
+
+    public void Move()
+    {
+        //log += "?";
+        for(int i = 0; i < drives.Count; i++)
+        {
+            Drive d = (Drive)drives[i];
+            d.new_position = new_position;
+            double en_req = d.energy_usage * Time.deltaTime;
+            //log += en_req.ToString();
+            //log += "<";
+            //log += energy.ToString();
+            //log += " ";
+            if ((en_req <= energy) && (d.is_firing == true))
+            {
+                //log += "!";
+                d.Fire();
+                
+            }
+            position = unityShip.transform.position;
+        }
+    }
+}
+
+    public class MainScript : MonoBehaviour {
+
+	
+
+	
+
+	public ArrayList enemy_ships;
+    public ArrayList my_ships;
+    Spaceship sp1;
+	// Use this for initialization
+	void Start () {
+        sp1 = new Spaceship(transform.gameObject, 100, 100);
+        sp1.new_position = new Vector3(-1, 1, 0);
+        Drive d1 = new Drive();
+        d1.is_firing = true;
+        d1.energy_usage = 1;
+        d1.speed = 2;
+        d1.unityShip = sp1.unityShip;
+        sp1.drives.Add(d1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        print(sp1.log);
+        if (Input.GetMouseButtonDown(0))
+        {
+            sp1.new_position = Input.mousePosition-sp1.position;
+        }
+        else
+        {
+            sp1.Move();
+            //print(Screen.height);
+        }
+	}
+}
---
+++ Assets/enmyscript.cs
@@ -0,0 +1,229 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class enemyScript : MonoBehaviour
+{
+    public int H;
+    public int W;
+    GameObject cont;
+    MainScript ms;
+    public int rangemy; // досигаемость оружия игрока
+    public int renge; // досигаемость оружия бота
+                      // Use this for initialization
+    void Start()
+    {
+        H = Screen.height;
+        W = Screen.width;
+        cont = GameObject.Find("Controller");
+        ms = cont.GetComponent<MainScript>();
+    }
+
+    void Update()
+    {
+        rangemy = 0;
+        float xy = 0;
+        Spaceship s1 = (Spaceship)ms.my_ships[0];
+        float x1 = s1.position.x;
+        float y1 = s1.position.y;
+        double nr1 = s1.new_rotation.z;
+        double r1 = System.Math.Asin(x1 / (x1 * x1) + (y1 * y1));
+        Weapon d11 = (Weapon)s1.weapons[0];
+        int d11_t = d11.d_type;
+        float rg11 = d11.w_range;
+        Weapon d12 = (Weapon)s1.weapons[1];
+        int d12_t = d12.d_type;
+        float rg12 = d12.w_range;
+        Spaceship s2 = (Spaceship)ms.enemy_ships[0];
+        float x2 = s2.position.x;
+        float y2 = s2.position.y;
+        double r2 = s2.rotation.z;
+        double nr2 = s2.new_rotation.z;
+        Weapon d21 = (Weapon)s2.weapons[0];
+        int d21_t = d21.d_type;
+        float rg21 = d21.w_range;
+        Weapon d22 = (Weapon)s2.weapons[1];
+        int d22_t = d22.d_type;
+        float rg22 = d22.w_range;
+        if (x1 > x2)
+        {
+            if (y1 > y2)
+            {
+                xy = ((x1 - x2) * (x1 - x2)) + ((y1 - y2) * (y1 - y2));
+            }
+            else if (y2 > y1)
+            {
+                xy = ((x1 - x2) * (x1 - x2)) + ((y2 - y1) * (y2 - y1));
+            }
+            else
+            {
+                xy = x1 - x2;
+            }
+        }
+        else if (x2 > x1)
+        {
+            if (y1 > y2)
+            {
+                xy = ((x2 - x1) * (x2 - x1)) + ((y1 - y2) * (y1 - y2));
+            }
+            else if (y2 > y1)
+            {
+                xy = ((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1));
+            }
+            else
+            {
+                xy = x2 - x1;
+            }
+        }
+        else
+        {
+            if (y1 > y2)
+            {
+                xy = y1 - y2;
+            }
+            else if (y2 > y1)
+            {
+                xy = y2 - y1;
+            }
+        }
+        r2 = Math.Abs(r2);
+        r1 = Math.Abs(r1);
+        if (System.Math.Sin(r2) > System.Math.Sin(r1))
+        {
+            if (r2 - r1 >= 180)
+            {
+                nr2 += ((360 - r2) + r1);
+            }
+            else
+            {
+                nr2 -= r2 - r1;
+            }
+        }
+        else if (System.Math.Sin(r2) < System.Math.Sin(r1))
+        {
+            if (r1 - r2 >= 180)
+            {
+                nr2 -= ((360 - r1) + r2);
+                nr2 = 360 - Math.Abs(nr2);
+            }
+            else
+            {
+                nr2 += r1 - r2;
+            }
+        }
+        if ((xy >= rg21) || (xy >= rg22))
+        {
+            if (xy >= rg21)
+            {
+                d21.is_firing = false;
+                if (x1 > x2)
+                {
+                    if (y1 > y2)
+                    {
+                        x2 += (x1 - x2) - 1;
+                        y2 += (y1 - y2) - 1;
+                    }
+                    else if (y2 > y1)
+                    {
+                        x2 += (x1 - x2) - 1;
+                        y2 -= ((y2 - y1) - 1);
+                    }
+                    else
+                    {
+                        x2 += (x1 - x2) - 1;
+                    }
+                }
+                else if (x2 > x1)
+                {
+                    if (y1 > y2)
+                    {
+                        x2 -= ((x2 - x1) - 1);
+                        y2 += ((y1 - y2) - 1);
+                    }
+                    else if (y2 > y1)
+                    {
+                        x2 -= ((x2 - x1) - 1);
+                        y2 -= ((y2 - y1) - 1);
+                    }
+                    else
+                    {
+                        x2 -= ((x2 - x1) - 1);
+                    }
+                }
+                else
+                {
+                    if (y1 > y2)
+                    {
+                        y2 += ((y1 - y2) - 1);
+                    }
+                    else if (y2 > y1)
+                    {
+                        y2 -= ((y2 - y1) - 1);
+                    }
+                }
+            }
+            else if (xy >= rg22)
+            {
+                d22.is_firing = false;
+                if (x1 > x2)
+                {
+                    if (y1 > y2)
+                    {
+                        x2 += (x1 - x2) - 1;
+                        y2 += (y1 - y2) - 1;
+                    }
+                    else if (y2 > y1)
+                    {
+                        x2 += (x1 - x2) - 1;
+                        y2 -= ((y2 - y1) - 1);
+                    }
+                    else
+                    {
+                        x2 += (x1 - x2) - 1;
+                    }
+                }
+                else if (x2 > x1)
+                {
+                    if (y1 > y2)
+                    {
+                        x2 -= ((x2 - x1) - 1);
+                        y2 += ((y1 - y2) - 1);
+                    }
+                    else if (y2 > y1)
+                    {
+                        x2 -= ((x2 - x1) - 1);
+                        y2 -= ((y2 - y1) - 1);
+                    }
+                    else
+                    {
+                        x2 -= ((x2 - x1) - 1);
+                    }
+                }
+                else
+                {
+                    if (y1 > y2)
+                    {
+                        y2 += ((y1 - y2) - 1);
+                    }
+                    else if (y2 > y1)
+                    {
+                        y2 -= ((y2 - y1) - 1);
+                    }
+                }
+            }
+            else if (xy < rg21)
+            {
+                d21.is_firing = true;
+            }
+            else if (xy < rg22)
+            {
+                d22.is_firing = true;
+            }
+        }
+        else if ((xy < rg21) && (xy < rg22))
+        {
+            d21.is_firing = true;
+            d22.is_firing = true;
+        }
+    }
+}
